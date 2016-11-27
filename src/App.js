cc88ccb3cc9b17ab9e79006a495fffb98045b7ac
Dan42K <jeanlouis.danielo@sfr.fr>
2016-11-27 23:01:49 +0100

    export layer comp, eject from react-app generator and add first component

--- src/App.js
+++ src/App.js
@@ -1,19 +1,15 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+
+import FlashMessage from './components/FlashMessage/index.js'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          <div className="hello">Hello</div>
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <FlashMessage message="Hello" type="error" />
+        <FlashMessage message="Hello" type="info" />
+        <FlashMessage message="Hello" type="success" />
       </div>
     );
   }
---
+++ src/components/FlashMessage/index.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import './FlashMessage.scss';
+
+import classNames from 'classnames';
+
+export default class FlashMessage extends Component {
+  constructor(props) {
+    super(props);
+    
+    
+    this.state = {
+      closed: false
+    }
+  }
+
+  componentDidMount() {
+    this.DOMRef = ReactDOM.findDOMNode(this);
+  }
+
+  /**
+   * Remove flash message
+   * @return null
+   */
+  remove() {
+    this.setState({closed: !this.state.closed});
+    setTimeout( () => {
+      this.DOMRef.parentNode.removeChild(this.DOMRef);
+    }, 750);
+  }
+
+  render() {
+    let { type, message } = this.props;
+
+    return (
+      <div className={ classNames('flash-message', 
+                                  type,
+                                  { 'closed': this.state.closed }) }>
+        <p>{ message || "Pas de message ?!" }</p>
+        <button title="Fermer élement" onClick={ () => this.remove() }></button>
+      </div>
+    );
+  }
+}
