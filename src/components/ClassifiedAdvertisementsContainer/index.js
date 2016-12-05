f63676ebde178b7d6ee57866c90b53525bafab1d
Dan42K <jeanlouis.danielo@sfr.fr>
2016-12-06 00:27:20 +0100

    add API management in cacontroller

--- src/components/ClassifiedAdvertisementsContainer/index.js
+++ src/components/ClassifiedAdvertisementsContainer/index.js
@@ -1,6 +1,6 @@
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
 
-// import APIManager from '../../utils/APIManager';
+import APIManager from '../../utils/APIManager';
 
 import ClassfiedAdvertisements from './../../fixtures/classified_advertisements.json';
 
@@ -8,6 +8,24 @@ import Pagination from '../Pagination'
 import ClassifiedAdvertisementsList from '../ClassifiedAdvertisementsList'
 
 export default class ClassifiedAdvertisementsContainer extends Component {
+
+  componentDidMount() {
+    // let isUserConnected = this.props.env == 'front' ?
+    // if (this.props.env == 'front') {
+      
+    // } else {
+    //   APIManager.getClassifiedAdvertisements(undefined, undefined, this.hello, this.hello2)
+    // }
+    APIManager.getClassifiedAdvertisements(undefined, undefined, this._getAdvertisementsSuccess, this._getAdvertisementsFail);
+  }
+
+  _getAdvertisementsSuccess(response) {
+    console.log(response);
+  }
+
+  _getAdvertisementsFail(response) {
+
+  }
 
   render() {
     // if (process.env.NODE_ENV === 'development') {
