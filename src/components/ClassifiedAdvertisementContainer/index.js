02aebf19a340126d5b711a9cb484c4c65efd4049
Dan42K <jeanlouis.danielo@sfr.fr>
2016-12-13 00:33:07 +0100

    fix too much recursion issue

--- src/components/ClassifiedAdvertisementContainer/index.js
+++ src/components/ClassifiedAdvertisementContainer/index.js
@@ -20,12 +20,20 @@ class ClassifiedAdvertisementContainer extends Component {
     }
   }
 
+
   componentDidMount() {
+    this.setState({ isLoading: true });
     APIManager.getClassifiedAdvertisement(this.props.params.id, this._getAdvertisementSuccess.bind(this), this._getAdvertisementFail.bind(this));
   }
 
-  componentDidUpdate() {
-    APIManager.getClassifiedAdvertisement(this.props.params.id, this._getAdvertisementSuccess.bind(this), this._getAdvertisementFail.bind(this));
+  componentDidUpdate(prevProps) {
+    let oldId = prevProps.params.page;
+    let currentId = this.props.params.page;
+
+    if (currentId !== oldId) {
+      this.setState({ isLoading: true });
+      APIManager.getClassifiedAdvertisement(currentId, this._getAdvertisementSuccess.bind(this), this._getAdvertisementFail.bind(this));
+    }
   }
 
   _getAdvertisementSuccess(response) {
@@ -37,11 +45,11 @@ class ClassifiedAdvertisementContainer extends Component {
   }
 
   _renderResults() {
-    if (this.state.APIDatas.data.resource) {
-      return this._renderClassifiedAdvertisement();
-    } else {
+    if (Object.keys(this.state.APIDatas).length === 0 || !this.state.APIDatas.data.resource) {
       return <NoResults message={'Cette annonce n\'existe pas'} />;
     }
+
+    return this._renderClassifiedAdvertisement();
   }
 
   _renderClassifiedAdvertisement() {
@@ -49,15 +57,6 @@ class ClassifiedAdvertisementContainer extends Component {
   }
 
   render() {
-    let formDatas = {}
-
-    // if (this.props.children && !this.state.isLoading) {
-    //   formDatas = this.state.APIDatas.data.resource;
-    //   formDatas.category = formDatas.category.id;
-    // }
-
-    // console.log( (this.props.children && !this.state.isLoading), this.props.children  )
-
     return (
       <div className="App">
         { !this.state.isLoading && this._renderResults() }
