074169753bbab79d24feac0266657d7fde083dcd
Dan42K <jeanlouis.danielo@sfr.fr>
2016-12-16 19:38:39 +0100

    transform InputFile stateless component to class es6

--- src/components/_Form/InputFile/index.js
+++ src/components/_Form/InputFile/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Component} from 'react';
 
 import uuid from 'node-uuid';
 // import Dropzone from 'react-dropzone'
@@ -9,59 +9,79 @@ import FormButton from '../FormButton'
 
 // http://stackoverflow.com/questions/36654641/file-attachments-for-redux-form-and-elixir-phoenix-as-backend-api-serialization
 
-const InputFile = function ({ input, fields, label, type, meta: { touched, error, warning, placeholder }, ...extras }) {
-  const id = uuid.v1();
-  let fileInput;
-  let reader = new FileReader();
+class InputFile extends Component {
+  constructor(props) {
+    super(props);
+    
+    this.state = {
+      hasImage: false
+    }
 
-  let removeImage = () => {
-    document.getElementById('uploadImage').setAttribute('src', '');
-    fileInput.setAttribute('value', '');
+    this.id = uuid.v1();
+    this.fileInput;
+    this.reader = new FileReader();
   }
 
-  let displayImageUploaded = ({ target }) => {
+  _displayImageUploaded = ({ target }) => {
 
     if (target.files && target.files[0]) {
       
-      reader.onload = function (e) {
+      this.reader.onload = function (e) {
         document.getElementById('uploadImage').setAttribute('src', e.target.result);
       }
       
-      window.foo = fileInput.files[0];
+      window.foo = this.fileInput.files[0];
 
-      reader.readAsDataURL(target.files[0]);
+      this.reader.readAsDataURL(target.files[0]);
     }
   }
 
-  return (
-    <div className='InputFile'>
-      <label htmlFor={id}>
-        <figure>
-          <img id='uploadImage' src={input.value} width="250" alt={ 'altImg' } />
-        </figure>
-      </label>
-      <div className='buttons-container fieldset'>
-        <FormButton design='cancel' text='Supprimer' type='button' onClick={ removeImage } />
+  _removeImage = () => {
+    document.getElementById('uploadImage').setAttribute('src', '');
+    this.fileInput.setAttribute('value', '');
+  }
+
+
+  render() {
+    let { input, fields, label, type, meta: { touched, error, warning, placeholder }, ...extras } = this.props;
+
+    return (
+      <div className='InputFile'>
+        <label htmlFor={this.id}>
+          <figure>
+            <img id='uploadImage' src={input.value} width="250" alt={ 'altImg' } />
+          </figure>
+        </label>
+        <div className='buttons-container fieldset'>
+          <FormButton design='cancel' text='Supprimer' type='button' onClick={ this._removeImage } />
+        </div>
+
+        {/*<Field name="file" component='input' type="hidden" />
+
+        <Dropzone
+            ref="dropzone"
+            onDrop={(upload) => dispatch(change('FileUploadExampleForm', 'file', upload[0]))}
+            multiple={false}
+            accept='image/*'>
+            <div>Click here select files to upload.</div>
+        </Dropzone>*/}
+
+        <input
+          id={this.id}
+          ref={(ref) => this.fileInput = ref}
+          {...input}
+          onChange={ this._displayImageUploaded }
+          type="file" />
       </div>
+    );
+  }
+}
 
-      {/*<Field name="file" component='input' type="hidden" />
 
-      <Dropzone
-          ref="dropzone"
-          onDrop={(upload) => dispatch(change('FileUploadExampleForm', 'file', upload[0]))}
-          multiple={false}
-          accept='image/*'>
-          <div>Click here select files to upload.</div>
-      </Dropzone>*/}
+// const InputFile = function () {
+  
 
-      <input
-        id={id}
-        ref={(ref) => fileInput = ref}
-        {...input}
-        onChange={ displayImageUploaded }
-        type="file" />
-    </div>
-  );
-};
+
+  
 
 export default InputFile;
